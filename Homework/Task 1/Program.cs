c380ec09d048468f4f866af6f0e6050ad13329cd
Ivan Kulikov <human.totem1@gmail.com>
2023-05-26 16:17:53 +0300

    2222

---
+++ Homework/Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 < 100)
+{
+    Console.WriteLine("Третьего числа нет");
+}
+else
+{
+    while (number1 > 999);
+    {
+        number1 = number1/10;
+    }
+    number1 = number1 % 100;
+    Console.WriteLine($"{number1}");
+}
